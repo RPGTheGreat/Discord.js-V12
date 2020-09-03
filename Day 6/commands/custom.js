3e69282526e485e673785893bc26494beade4c50
RPGTheGreat <54214792+RPGTheGreat@users.noreply.github.com>
2020-09-03 21:48:25 +0530

    Update custom.js

--- Day 6/commands/custom.js
+++ Day 6/commands/custom.js
@@ -8,4 +8,7 @@ if (!args[0]) return message.channel.send(`GIVE A COMMAND`)
 if (!args.slic(1).join(" ")) return message.channel.send(`GIVE A TRIGGER`)
 
 db.set(`cmd_${message.guild.id}`, args[0]
-message.channel.sen
+db.set(`tri_${message.guild.id}`, args.slice(1).join(" "))
+message.channel.send(`COMMAND CREATED \`${args[0]}\``)
+  }
+  }
